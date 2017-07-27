dedecb77887012b56dfbceaece72d14b8b6f1007
Catherine <catherine.olaguir@gmail.com>
2017-07-27 10:59:03 +0800

    Signed-off-by: Catherine <catherine.olaguir@gmail.com>

--- SampleSystem/MainWindow.xaml.cs
+++ SampleSystem/MainWindow.xaml.cs
@@ -81,6 +81,7 @@ namespace SampleSystem
 
         private void onLoadBooks()
         {
+            
             SqlConnect con = new SqlConnect();
             con.conOpen();
             if (con == null)
@@ -89,39 +90,46 @@ namespace SampleSystem
             }
             else
             {
-                //SELECT A.*, (B.author_fname + ' ' + B.author_lname) AS Author FROM tblBooks A INNER JOIN tblAuthor B ON A.bookauthor_id = B.authorid
                 string query = "SELECT A.*, (B.author_fname + ' ' + B.author_lname) AS Author FROM tblBooks A INNER JOIN tblAuthor B ON A.bookauthor_id = B.authorid";
                 SqlCommand cmd = new SqlCommand(query, con.Con);
-                //SqlDataReader sdr = cmd.ExecuteReader();
                 SqlDataAdapter sda = new SqlDataAdapter(cmd);
                 DataSet ds = new DataSet();
                 DataTable dt = new DataTable();
                 sda.Fill(dt);
-                
-                //if (sdr.HasRows)
-                //if(sda.Fill(dt) > 0)
-                //{
-                    Console.WriteLine(sda.Fill(dt));
-                    //Console.WriteLine(dt.DefaultView.ToTable(true, "bookid", "bookauthorid"));
-                    //  DataTable dt = new DataTable();
-                    //dt.Clear();
-                    //gridDataLoading.ItemsSource = dt.DefaultView;
-                    gridDataLoading.DataContext = dt.DefaultView;
-                    gridDataLoading.Columns[0].Visibility = Visibility.Collapsed;
-                    gridDataLoading.Columns[2].Visibility = Visibility.Collapsed;
-                    gridDataLoading.Columns[1].Header = "Book Title";
-                    gridDataLoading.Columns[3].Header = "Date Published";
-                    gridDataLoading.Columns[4].Header = "Quantity";
-                    gridDataLoading.Columns[5].Header = "Stocks";
-                    gridDataLoading.CanUserAddRows = false;
-
-                    //sda.Fill(dt);
-                    //gridDataLoading.ItemsSource = dt;
-                //}
+                gridDataLoading.ItemsSource = dt.DefaultView;
+                gridDataLoading.Columns[0].Visibility = Visibility.Collapsed;
+                gridDataLoading.Columns[2].Visibility = Visibility.Collapsed;
+                gridDataLoading.Columns[1].Header = "Book Title";
+                gridDataLoading.Columns[3].Header = "Date Published";
+                gridDataLoading.Columns[4].Header = "Quantity";
+                gridDataLoading.Columns[5].Header = "Stocks";
+                gridDataLoading.CanUserAddRows = false;
                 con.conclose();
             }
-
             
+        }
+
+        private void onLoadAuthor()
+        {
+            SqlConnect con = new SqlConnect();
+            con.conOpen();
+            if(con == null)
+            {
+                return;
+            }
+            else
+            {
+
+                string q = "SELECT * FROM tblAuthor";
+                SqlCommand cmd = new SqlCommand(q, con.Con);
+                SqlDataAdapter sda = new SqlDataAdapter(cmd);
+                DataSet ds = new DataSet();
+                DataTable dt = new DataTable();
+                sda.Fill(dt);
+                gridDataLoading.DataContext = dt.DefaultView;
+                gridDataLoading.Columns[0].Visibility = Visibility.Collapsed;
+
+            }
         }
 
         private void btnUserWindow_Click(object sender, RoutedEventArgs e)
